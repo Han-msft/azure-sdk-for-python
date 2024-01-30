3b255e61e0c066715c3ff9c49124c4811d7b6458
Han Chiang <hachiang@microsoft.com>
2024-01-30 17:28:00 +0800

    add session test and modify sample

--- sdk/face/azure-ai-face/samples/sample_liveness_session.py
+++ sdk/face/azure-ai-face/samples/sample_liveness_session.py
@@ -24,7 +24,11 @@ import os
 from pathlib import Path
 from azure.core.credentials import AzureKeyCredential
 from azure.ai.face import FaceClient
-from azure.ai.face.models import LivenessSessionParameters, LivenessWithVerifySessionParameters
+from azure.ai.face.models import (
+    LivenessSessionParameters,
+    LivenessWithVerifySessionParameters,
+    LivenessWithVerifySessionCreateContent,
+)
 from dotenv import find_dotenv, load_dotenv
 
 TEST_DEVICE_CORELATION_ID = "dummy-test-id"
@@ -51,9 +55,15 @@ if __name__ == "__main__":
     print(f"Liveness session {creation_result.session_id} deleted.")
 
     sample_path = Path(__file__).resolve().parent / "sample.jpg"
+    parameters = LivenessWithVerifySessionParameters(
+        device_correlation_id=TEST_DEVICE_CORELATION_ID,
+        liveness_operation_mode=TEST_LIVENESS_OPERATION_MODE,
+    )
     with open(sample_path, "rb") as fd:
         file = fd.read()
-    sessionWithVerify = LivenessWithVerifySessionParameters(parameters=parameters, verify_image=file)
+    sessionWithVerify = LivenessWithVerifySessionCreateContent(
+        parameters=parameters, verify_image=("verify_image.jpg", file, "base64")
+    )
 
     creation_result = operations.create_session_with_verify_image(sessionWithVerify)
     print(f"Liveness session created with ID {creation_result.session_id}.")
---
+++ sdk/face/azure-ai-face/tests/test_sessions_async.py
@@ -0,0 +1,106 @@
+# coding: utf-8
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+from azure.ai.face.aio import FaceClient
+from azure.core.credentials import AzureKeyCredential
+from devtools_testutils.aio import recorded_by_proxy_async
+from test_case import FaceTest, FacePreparer
+
+
+class TestSessionsAsync(FaceTest):
+    def _create_client(self, **kwargs):
+        endpoint = kwargs.pop("face_endpoint")
+        key = kwargs.pop("face_key")
+        face_client = self.create_client_from_credential(
+            FaceClient, credential=AzureKeyCredential(key), endpoint=endpoint
+        )
+        return face_client
+
+    @FacePreparer()
+    @recorded_by_proxy_async
+    async def test_create_get_delete_liveness_session(self, **kwargs):
+        client = self._create_client(**kwargs)
+        content = self._build_liveness_session_creation_content()
+        result = await client.sessions.create_liveness_session(content)
+        self._validate_session_creation_result(result)
+
+        session_id = result.session_id
+        print(session_id)
+        session = await client.sessions.get_liveness_session(session_id)
+        self._validate_new_session_get_result(session, session_id)
+
+        await client.sessions.delete_liveness_session(session_id)
+
+    @FacePreparer()
+    @recorded_by_proxy_async
+    def test_create_get_delete_liveness_with_verify_session_form_data(self, **kwargs):
+        client = self._create_client(**kwargs)
+        content = self._build_liveness_session_with_verify_creation_content()
+        result = client.create_liveness_with_verify_session_by_form_data(content)
+        self._validate_session_creation_result(result)
+
+        session_id = result.session_id
+        print(session_id)
+        session = client.get_liveness_with_verify_session(session_id)
+        self._validate_new_session_get_result(session, session_id)
+
+        client.delete_liveness_with_verify_session(session_id)
+
+    @FacePreparer()
+    @recorded_by_proxy_async
+    def test_create_get_delete_liveness_with_verify_session_json(self, **kwargs):
+        client = self._create_client(**kwargs)
+        content = self._build_liveness_session_creation_content()
+        result = client.create_liveness_with_verify_session_by_json(content)
+        self._validate_session_creation_result(result)
+
+        session_id = result.session_id
+        print(session_id)
+        session = client.get_liveness_with_verify_session(session_id)
+        self._validate_new_session_get_result(session, session_id)
+
+        client.delete_liveness_with_verify_session(session_id)
+
+    @FacePreparer()
+    @recorded_by_proxy_async
+    def test_get_liveness_session_audit_entries(self, **kwargs):
+        session_id = "3b8b94dc-c398-4dc5-85ae-f7d721648d18"
+        number_of_entries = 3
+        top_limit = 1
+
+        client = self._create_client(**kwargs)
+        entry_list = client.list_liveness_session_audit_entries(session_id)
+        assert len(entry_list) == number_of_entries
+
+        for entry in entry_list:
+            self._validate_liveness_session_audit_entry(entry, session_id)
+
+        entry_list_with_query = client.list_liveness_session_audit_entries(
+            session_id, start=entry_list[0].id, top=top_limit
+        )
+        assert len(entry_list_with_query) == top_limit
+        assert entry_list_with_query[0].id == entry_list[1].id
+
+    @FacePreparer()
+    @recorded_by_proxy_async
+    def test_get_liveness_sessions(self, **kwargs):
+        number_of_sessions = 3
+        client = self._create_client(**kwargs)
+        session_ids = []
+
+        try:
+            content = self._build_liveness_session_creation_content()
+            for i in range(number_of_sessions):
+                result = client.create_liveness_session(content)
+                session_ids.append(result.session_id)
+
+            session_list = client.list_liveness_sessions()
+            for session in session_list:
+                self._validate_liveness_session_item(session)
+        finally:
+            for id in session_ids:
+                client.delete_liveness_session(id)
